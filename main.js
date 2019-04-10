bca43d7db7d76c73f1bf487db62be009b7709c0d
Ashton Snelgrove <asnelgrove@sofi.org>
2019-04-10 14:44:02 -0600

    Fix x and y orientation

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 const box = document.getElementById('box');
-
-const bound_x = 32;
-const bound_y = 32;
-var field =  new Array(bound_x).fill(0).map(() => new Array(bound_y).fill(0));
-
 const random_int = (max) => Math.floor(Math.random() * Math.floor(max));
 
 const UP = [-1,0];
@@ -11,45 +6,47 @@ const DOWN =  [1,0];
 const RIGHT = [0,1];
 const LEFT = [0,-1];
 
-let x = random_int(bound_x);
-let y = random_int(bound_y);
-let l = 2;
-let d = UP;
-let fx = random_int(bound_x);
-let fy = random_int(bound_y);
-let running = true;
+const bound_x = 32;
+const bound_y = 32;
 
-
-const feed = () => {
-  while (field[fx][fy] !== 0) {
-    fx = random_int(bound_x);
-    fy = random_int(bound_y);
-  }
+const feed = (game) => {
+  do {
+    game.fx = random_int(bound_x);
+    game.fy = random_int(bound_y);
+  } while (game.field[game.fy][game.fx] !== 0)
+  return game; // todo make immutable
 };
 
 const setup = () => {
+  let game = {}
+  game.field =  new Array(bound_y).fill(0).map(() => new Array(bound_x).fill(0));
+  game.x = random_int(bound_x);
+  game.y = random_int(bound_y);
+  game.l = 2;
+  game.d = UP;
+  game.running = true;
+
   switch (random_int(4)) {
-  case 0: d = UP; break;
-  case 1: d = DOWN; break;
-  case 2: d = LEFT; break;
-  case 3: d = RIGHT; break;
+  case 0: game.d = UP; break;
+  case 1: game.d = DOWN; break;
+  case 2: game.d = LEFT; break;
+  case 3: game.d = RIGHT; break;
   }
 
-  for (let i = l; i > 0; i--) {
-    field[x - (i * d[0])][y - (i * d[1])] = i;
+  for (let i = game.l; i > 0; i--) {
+    game.field[game.y - (i * game.d[0])][game.x - (i * game.d[1])] = i;
   }
-  feed();
+  return feed(game);
 }
 
-
-const draw = () => {
+const draw = (game) => {
   box.innerHTML = "";
-  for (var i = 0; i < bound_x; i++) {
+  for (var i = 0; i < bound_y; i++) {
     box.innerHTML += "<p>";
-    for (var j = 0; j < bound_y; j++) {
-      if (field[i][j] > 0)
+    for (var j = 0; j < bound_x; j++) {
+      if (game.field[i][j] > 0)
         box.innerHTML += '+';
-      else if (i === fx && j === fy)
+      else if (i === game.fx && j === game.fy)
         box.innerHTML += '0';
       else
         box.innerHTML += '.';
@@ -58,44 +55,49 @@ const draw = () => {
   }
 }
 
+const update_game = (game) => {
+  nx = game.x + game.d[0]
+  ny = game.y + game.d[1]
+  if (nx >= bound_x || nx < 0 || ny >= bound_y || ny < 0 || game.field[nx][ny] > 0) {
+    game.running = false;
+  } else if (nx === game.fx && ny == game.fy) {
+    game.l += 1;
+    game.x = nx;
+    game.y = ny;
+    game.field[nx][ny] = game.l;
+    feed(game);
+  } else {
+    game.x = nx;
+    game.y = ny;
+    for (var i = 0; i < bound_x; i++) {
+      for (var j = 0; j < bound_y; j++) {
+        game.field[i][j] = Math.max(0, game.field[i][j] - 1);
+      }
+    }
+    game.field[game.x][game.y] = game.l;
+  }
+  return game; // todo make immutable
+}
+
+let game = setup();
 
 document.addEventListener('keydown', function(event) {
   if(event.keyCode == 37) {
-    d = LEFT;
+    game.d = LEFT;
   } else if(event.keyCode == 38) {
-    d = UP;
+    game.d = UP;
   } else if(event.keyCode == 39) {
-    d = RIGHT;
+    game.d = RIGHT;
   } else if(event.keyCode == 40) {
-    d = DOWN;
+    game.d = DOWN;
   }
 });
 
 let tick = 0;
 const update = () => {
-  tick++;
-  if (tick === 30) {
+  if (tick++ === 15) {
     tick = 0;
-    nx = x + d[0]
-    ny = y + d[1]
-    if (nx >= bound_x || nx < 0 || ny >= bound_y || ny < 0 || field[nx][ny] > 0) {
-      running = false;
-    } else if (nx === fx && ny == fy) {
-      l += 1;
-      x = nx;
-      y = ny;
-      field[nx][ny] = l;
-      feed();
-    } else {
-      x = nx;
-      y = ny;
-      for (var i = 0; i < bound_x; i++) {
-        for (var j = 0; j < bound_y; j++) {
-          field[i][j] = Math.max(0, field[i][j] - 1);
-        }
-      }
-      field[x][y] = l;
-    }
+    game = update_game(game);
   }
 }
 
@@ -109,38 +111,36 @@ let fps = 60;
 let framesThisSecond = 0;
 let lastFpsUpdate = 0;
 
-const panic = () => {
-  running = false;
-}
+const panic = () => {}
 
 const mainLoop = (timestamp) => {
-    // Throttle the frame rate.
-    if (timestamp < lastFrameTimeMs + (1000 / maxFPS)) {
-        requestAnimationFrame(mainLoop);
-        return;
-    }
-    delta += timestamp - lastFrameTimeMs;
-    lastFrameTimeMs = timestamp;
-
-    if (timestamp > lastFpsUpdate + 1000) {
-        fps = 0.25 * framesThisSecond + 0.75 * fps;
-
-        lastFpsUpdate = timestamp;
-        framesThisSecond = 0;
-    }
-    framesThisSecond++;
-
-    let numUpdateSteps = 0;
-    while (delta >= timestep) {
-        update(timestep);
-        delta -= timestep;
-        if (++numUpdateSteps >= 240) {
-            panic();
-            break;
-        }
-    }
-    draw();
+  // Throttle the frame rate.
+  if (timestamp < lastFrameTimeMs + (1000 / maxFPS)) {
     requestAnimationFrame(mainLoop);
+    return;
+  }
+  delta += timestamp - lastFrameTimeMs;
+  lastFrameTimeMs = timestamp;
+
+  if (timestamp > lastFpsUpdate + 1000) {
+    fps = 0.25 * framesThisSecond + 0.75 * fps;
+
+    lastFpsUpdate = timestamp;
+    framesThisSecond = 0;
+  }
+  framesThisSecond++;
+
+  let numUpdateSteps = 0;
+  while (delta >= timestep) {
+    update(game);
+    delta -= timestep;
+    if (++numUpdateSteps >= 240) {
+      panic();
+      break;
+    }
+  }
+  draw(game);
+  requestAnimationFrame(mainLoop);
 }
 
 requestAnimationFrame(mainLoop);
